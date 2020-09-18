811aa087c8085bfa02fe80ee5f9d5dfe9f04bc2f
klequis <klequis@gmail.com>
2020-09-18 13:05:19 -0700

    -wip

--- src/components/TextEdit.jsx
+++ src/components/TextEdit.jsx
@@ -1,38 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import Form from 'react-bootstrap/Form'
-import { createUseStyles, useTheme } from 'react-jss'
+// import { createUseStyles, useTheme } from 'react-jss'
+import styled from 'styled-components'
 
 // eslint-disable-next-line
 import { green, redf } from 'logger'
 import { props } from 'ramda'
 
-const useStyles = createUseStyles(theme => ({
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  feedback: {
-    marginLeft: 4,
-    fontSize: '0.8375rem',
-    color: theme.colors.text.danger
-  },
-  controlInvalid: {
-    backgroundColor: theme.colors.background.danger
-  }
-}))
+// const useStyles = createUseStyles(theme => ({
+//   wrapper: {
+//     display: 'flex',
+//     flexDirection: 'column'
+//   },
+//   feedback: {
+//     marginLeft: 4,
+//     fontSize: '0.8375rem',
+//     color: theme.colors.text.danger
+//   },
+//   controlInvalid: {
+//     backgroundColor: theme.colors.background.danger
+//   }
+// }))
 
+const Wrapper = styled.div`
+  display: 'flex';
+  flexDirection: 'column';
+`;
+
+const FormControl = styled(Form.Control)`
+  height: calc(1.5em + 0.35rem + 2px);
+  padding-top: 0;
+  padding-bottom: 0;
+  padding-right: 0;
+  font-size: 0.675rem;
+  &is-invalid {
+    background-color: red;
+  }
+  
+  
+`
 
 const makeStyle = (maxWidth, minWidth) => {
-  const textStyle = {
-    height: 'calc(1.5em + 0.35rem + 2px)',
-    paddingTop: 0,
-    paddingBottom: 0,
-    paddingRight: 0,
-    fontSize: '0.675rem'
-  }
+  // const textStyle = {
+  //   height: 'calc(1.5em + 0.35rem + 2px)',
+  //   paddingTop: 0,
+  //   paddingBottom: 0,
+  //   paddingRight: 0,
+  //   fontSize: '0.675rem'
+  // }
   const max = maxWidth ? { maxWidth } : {}
   const min = minWidth ? { minWidth } : {}
-  return { ...textStyle, ...max, ...min }
+  return { /*...textStyle,*/ ...max, ...min }
 }
 
 const TextEdit = ({
@@ -50,8 +68,7 @@ const TextEdit = ({
   const [_isMinLength, _setIsMinLength] = useState(true)
   const [_isValid, _setIsValid] = useState(true)
 
-  const theme = useTheme()
-  const classes = useStyles({ ...props, theme })
+  const classes = {}
   const style = makeStyle(maxWidth, minWidth)
 
   const _handleChange = (event) => {
@@ -73,10 +90,10 @@ const TextEdit = ({
   }, [_setIsValid, _touched, _isMinLength])
 
   return (
-    <div className={classes.wrapper}>
-      <Form.Control
-        style={style}
-        className={_isValid ? '' : classes.controlInvalid}
+    <Wrapper>
+      <FormControl
+        // style={style}
+        // className={_isValid ? '' : classes.controlInvalid }
         type="text"
         name={name}
         value={_value}
@@ -85,12 +102,14 @@ const TextEdit = ({
         onBlur={_handleBlur}
         placeholder={placeholder}
         disabled={disabled}
+        invalid={true}
+        isInvalid={true}
       />
       {
         _isValid ? null : <span className={classes.feedback}>Minimum 3 characters</span>
       }
       
-    </div>
+    </Wrapper>
   )
 }
 
