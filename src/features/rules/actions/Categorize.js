8ab321d4d1ab8221e5a2c7cf765a1c4a156e0709
klequis <klequis@gmail.com>
2020-11-06 10:51:41 -0800

    code cleanup

--- src/features/rules/actions/Categorize.js
+++ src/features/rules/actions/Categorize.js
@@ -9,41 +9,22 @@ import { transactionFields as tFields } from 'features/transactions'
 // eslint-disable-next-line
 import { green, redf, purple } from 'logger'
 
-// const Wrapper = styled.div`
-//   // display: flex;
-  
-//   width: 100%;
-// `
-// background-color: red;
-
 export const Categorize = ({ action, minChars }) => {
   const [_action, _setAction] = useState(action)
   const { category1, category2 } = _action
   const dispatch = useDispatch()
 
-
-
-
   const _handleEvent = (event) => {
     const { name, value } = event.target
     const { type: eventType } = event
     console.group('_handleEvent')
-    // green('name', name)
-    // green('value', value)
-    // green('eventType', eventType)
-    
     const newAction = R.mergeRight(_action, { [name]: value })
-    // green('_handleEvent: newAction', newAction)
     console.groupEnd()
     _setAction(newAction)
-
     if (eventType === 'blur') {
-      // green('typeIsBlur')
       dispatch(ruleEditActionUpdate(newAction))
     }
-    
   }
-
 
   return (
     <>
