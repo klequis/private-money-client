79488262a9df2504190d4c5b4660b37b01a05deb
Derek <derekhaefner@gmail.com>
2020-12-23 14:13:55 -0500

    Add Rule Edit heading"

--- src/features/rules/Rule/RuleToolbar.js
+++ src/features/rules/Rule/RuleToolbar.js
@@ -5,8 +5,15 @@ import { Button } from 'components/Button'
 import { selectRuleEditIsDirty } from 'features/selectors'
 
 const RuleToolbarDiv = styled.div`
+  display: flex;
+  align-items: center;
   padding-top: 16px;
   padding-bottom: 16px;
+`
+
+const RuleToolbarH4 = styled.h4`
+  margin-right: 12px;
+  margin-bottom: 0;
 `
 
 const Btn = styled(Button)`
@@ -17,6 +24,7 @@ export const RuleToolbar = ({ save, cancel, deleteRule }) => {
   const _dirty = useSelector(selectRuleEditIsDirty)
   return (
     <RuleToolbarDiv>
+      <RuleToolbarH4>Rule Edit</RuleToolbarH4>
       <Btn onClick={save} disabled={!_dirty}>
         Save
       </Btn>
