c2823e118873312314d3e1942207c5f908bedeea
Derek <derekhaefner@gmail.com>
2020-12-24 17:12:49 -0500

    Increase TxTblHead font-size

--- src/features/tx/TxTable/TxColHead.js
+++ src/features/tx/TxTable/TxColHead.js
@@ -7,7 +7,9 @@ import { useDebouncedCallback } from 'use-debounce'
 import classNames from 'classnames'
 import { SortIcons } from './SortIcons'
 
-import { selectTxTblFilterValue } from 'features/selectors'
+import {
+  selectTxTblFilterValue
+} from 'features/selectors'
 
 // eslint-disable-next-line
 import { green, purple, grpStart, grpEnd } from 'logger'
@@ -17,27 +19,21 @@ const TextInput = styled.input`
   height: 24px;
 `
 
+
+
 const ColNameDiv = styled.div`
   display: flex;
-  justify-content: ${(props) =>
-    props.align === 'right' ? 'flex-end' : 'flex-start'};
+  justify-content: flex-start;
   align-items: center;
 `
 
-<<<<<<< add-income-expense-options-issue-25:src/features/txTbl/TxTbl/TxTblColHead.js
-const FieldDescriptionDiv = styled.div`
-  self-align: flex-start;
-  margin-right: 15px;
-  font-size: 20px;
-=======
 const FieldNameDiv = styled.div`
  self-align: flex-start;
  margin-right: 15px;
  font-size: 20px;
->>>>>>> Increase TxTblHead font-size:src/features/tx/TxTable/TxColHead.js
 `
 
-export const TxTblColHead = ({ fieldName, align, fieldDescription }) => {
+export const TxColHead = ({ fieldName }) => {
   const _dispatch = useDispatch()
 
   // const [_value, _setValue] = useState('')
@@ -53,9 +49,7 @@ export const TxTblColHead = ({ fieldName, align, fieldDescription }) => {
     1000
   )
 
-  const filterValue = useSelector((state) =>
-    selectTxTblFilterValue(fieldName, state)
-  )
+  const filterValue = useSelector(state => selectTxTblFilterValue(fieldName, state))
 
   const _onTextInputChange = (e) => {
     const value = e.target.value
@@ -66,28 +60,9 @@ export const TxTblColHead = ({ fieldName, align, fieldDescription }) => {
     _dispatch(updateSort(sortObj))
   }
 
+
   return (
     <th>
-<<<<<<< add-income-expense-options-issue-25:src/features/txTbl/TxTbl/TxTblColHead.js
-      {fieldName !== txFields.omit.name ? (
-        <div>
-          <ColNameDiv align={align}>
-            <FieldDescriptionDiv>{fieldDescription}</FieldDescriptionDiv>
-            <SortIcons fieldName={fieldName} onChange={_onSortIconsChange} />
-          </ColNameDiv>
-          <TextInput
-            className={classNames(['form-control', 'form-control-sm'])}
-            type="text"
-            value={filterValue}
-            onChange={_onTextInputChange}
-          />
-        </div>
-      ) : (
-        <ColNameDiv align={align}>
-          <FieldDescriptionDiv>{fieldDescription}</FieldDescriptionDiv>
-        </ColNameDiv>
-      )}
-=======
       {
         fieldName !== txFields.omit.name ? (
           <div>
@@ -109,7 +84,6 @@ export const TxTblColHead = ({ fieldName, align, fieldDescription }) => {
             <FieldNameDiv>{fieldName}</FieldNameDiv>
           )
       }
->>>>>>> Increase TxTblHead font-size:src/features/tx/TxTable/TxColHead.js
     </th>
   )
 }
