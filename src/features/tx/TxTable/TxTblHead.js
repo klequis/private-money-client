e61fb263c9263a2ddd5e668f90bea27d26a6c34c
Derek <derekhaefner@gmail.com>
2020-12-24 18:04:39 -0500

    Left align date, acctId, and description. Right align amount, cat1, cat2, type, and omit

--- src/features/tx/TxTable/TxTblHead.js
+++ src/features/tx/TxTable/TxTblHead.js
@@ -3,17 +3,18 @@ import { TxColHead } from './TxColHead'
 import { txFields } from 'features/tx'
 
 export const TxTblHead = () => {
+  console.log(txFields);
   return (
     <thead>
       <tr>
-        <TxColHead fieldName={txFields.date.name} />
-        <TxColHead fieldName={txFields.acctId.name} />
-        <TxColHead fieldName={txFields.description.name} />
-        <TxColHead fieldName={txFields.amount.name} />
-        <TxColHead fieldName={txFields.category1.name} />
-        <TxColHead fieldName={txFields.category2.name} />
-        <TxColHead fieldName={txFields.type.name} />
-        <TxColHead fieldName={txFields.omit.name} />
+        <TxColHead fieldName={txFields.date.name} align="left" />
+        <TxColHead fieldName={txFields.acctId.name} align="left" />
+        <TxColHead fieldName={txFields.description.name} align="left" />
+        <TxColHead fieldName={txFields.amount.name} align="right" />
+        <TxColHead fieldName={txFields.category1.name} align="right" />
+        <TxColHead fieldName={txFields.category2.name} align="right" />
+        <TxColHead fieldName={txFields.type.name} align="right" />
+        <TxColHead fieldName={txFields.omit.name} align="right" />
       </tr>
     </thead>
   )
