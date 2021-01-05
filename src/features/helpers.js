1db3ff54bde0b0c8f93ba8d4ed5b3b8ecbbc7a25
klequis <klequis@gmail.com>
2021-01-05 15:55:34 -0800

    wip

--- src/features/helpers.js
+++ src/features/helpers.js
@@ -104,10 +104,10 @@ export const createNewState = R.curry((path, newValue, state) => {
  * @returns {any} returns whatever is in state
  */
 export const getStateValue = (path, state) => {
-  grpStart('getStateValue')
-  blue('path', path)
-  blue('state', state)
-  grpEnd()
+  // grpStart('getStateValue')
+  // blue('path', path)
+  // blue('state', state)
+  // grpEnd()
   const root = path[0]
   const ret = R.has(root)(state)
     ? R.path(path, state)
