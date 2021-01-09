8cb4050c32f1c3aa89a2aa0963a23af72a9f600a
klequis <klequis@gmail.com>
2021-01-08 16:37:55 -0800

    remove logging

--- src/features/rules/rulesSlice.js
+++ src/features/rules/rulesSlice.js
@@ -265,27 +265,19 @@ const rulesSlice = createSlice({
      * @returns {object} the new state
      */
     ruleEditCriterionUpdate(state, action) {
-      // grpStart('ruleEditCriterionUpdate')
       const currState = current(state)
-      // blue('currState', currState)
       const newCriterion = R.path([wdPayload], action)
-      // blue('newCriterion', newCriterion)
       const newCriterionId = R.prop(wdId, newCriterion)
-      // blue('newCriterionId', newCriterionId)
       const currCriteria = selectRuleEditCriteria(currState)
-      // blue('currCriteria', currCriteria)
       const idxOfCriteriaToReplace = R.findIndex(
         R.propEq(wdId, newCriterionId)
       )(currCriteria)
-      // blue('idxOfCriteriaToReplace', idxOfCriteriaToReplace)
 
       const newCriteria = R.update(
         idxOfCriteriaToReplace,
         newCriterion,
         currCriteria
       )
-      // blue('newCriteria', newCriteria)
-      // grpEnd()
       return R.pipe(_criteriaSet(newCriteria), _isDirtySet(true))(currState)
     },
     /**
