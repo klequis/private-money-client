3ed9b1cca4f8b40a285ef817b74e619605915acb
klequis <klequis@gmail.com>
2021-05-27 17:02:37 -0700

    experimental code

---
+++ src/lib/getMonthIndex.js
@@ -0,0 +1,16 @@
+const months = {
+  jan: 0,
+  feb: 1,
+  mar: 2,
+  apr: 3,
+  may: 4,
+  jun: 5,
+  jul: 6,
+  aug: 7,
+  sep: 8,
+  oct: 9,
+  nov: 10,
+  dec: 11
+}
+
+export const getMonthIndex = (month) => months[month]
