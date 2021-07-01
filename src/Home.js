f0bfa329fac06254e750c5ed86d64d5729339071
klequis <klequis@gmail.com>
2021-07-01 10:58:46 -0700

    code formatting

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Upload } from 'features/upload'
 import { Transactions } from './Transactions'
+
 export const Home = () => {
   return (
     <Router>
